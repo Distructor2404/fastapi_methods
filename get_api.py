3d6a392485c2bb3aedfac777eb4e281801d7003b
Distructor2404 <abhishek123kumar123singh@gmail.com>
2024-01-28 02:10:03 +0530

    get_method

--- get_api.py
+++ get_api.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+app = FastAPI()
+import uvicorn
+
+@app.get("/items/")
+def read_items():
+    return [{"First name": "Abhishek Kumar"}, {"Last name": "Singh"}]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
