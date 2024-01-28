f519c3c000630ea526b7a12f83b5b87dc6f448e3
Distructor2404 <abhishek123kumar123singh@gmail.com>
2024-01-28 23:05:26 +0530

    First commit

--- post_api.py
+++ post_api.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 app = FastAPI()
 import uvicorn
 
-@app.get("/items/get")
+@app.post("/items/post")
 def read_items():
     return [{"First name": "Abhishek Kumar"}, {"Last name": "Singh"}]
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
