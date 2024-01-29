ef302e9e108c9af4b76c7b98736d08fe23d57066
Distructor2404 <abhishek123kumar123singh@gmail.com>
2024-01-29 22:13:47 +0530

    string_parameters2

--- path_parameter.py
+++ path_parameter.py
@@ -4,6 +4,12 @@ app = FastAPI()
 
 @app.get("/animal/{animal_name}") 
 async def get_animal(animal_name: str):
-    return {"animal_id": animal_name } 
+    return {"animal_id": animal_name } #accept only string
 
+@app.get("/user/me")
+async def read_user_me():
+    return {"user_id": "the current user"} #accept any value
 
+@app.get("/user/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id} #accepts only integer
